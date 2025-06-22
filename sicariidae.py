83c3b020d299c81ff32247e6a2eeae0d71edec2e
Z3r0X <cybersmart404@gmail.com>
2025-06-22 15:06:06 +0000

    Update sicariidae.py

--- sicariidae.py
+++ sicariidae.py
@@ -1,14 +1,34 @@
-#Sicariidae </Z3r0X>
-#Note: this program is for testing and educational purposes only
+#Sicariidae Web Crawler   </Z3r0X>
+#Note: this tool is open source, for testing and educational usage only!
 import sys
 import random
-import aiohttp                                                  import asyncio
+import aiohttp
+import asyncio
 import pyfiglet
-from bs4 import BeautifulSoup                                   from cache import CacheHandler, Clear                           from colorama import Fore, Style, init
-from urllib.parse import urlparse                                                                                               init(autoreset=True)                                                                                                            #colors                                                         red = Fore.RED                                                  blue = Fore.BLUE
-yellow = Fore.YELLOW                                            green = Fore.GREEN                                              white = Fore.WHITE                                              cyan = Fore.CYAN                                                                                                                class WebScraper:                                                   def __init__(self):                                                 self.cache = CacheHandler()                                     self.user_agents = [                                                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+from bs4 import BeautifulSoup
+from cache import CacheHandler, Clear
+from colorama import Fore, Style, init
+from urllib.parse import urlparse
+
+init(autoreset=True)
+
+#colors
+red = Fore.RED
+blue = Fore.BLUE
+yellow = Fore.YELLOW
+green = Fore.GREEN
+white = Fore.WHITE
+cyan = Fore.CYAN
+
+class WebScraper:
+    def __init__(self):
+        self.cache = CacheHandler()
+        self.user_agents = [
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
             "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:113.0) Gecko/20100101 Firefox/113.0",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15",                                                                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",                                                                "Mozilla/5.0 (Linux; Android 11; SM-A515F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0",
+            "Mozilla/5.0 (Linux; Android 11; SM-A515F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36",
             "Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Mobile/15E148 Safari/604.1"
         ]
 
